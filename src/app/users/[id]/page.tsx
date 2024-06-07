9b16fdf952d8ee8a9648c231ec4a82981d44e55f
mertcantacyldz <93483573+mertcantacyldz@users.noreply.github.com>
2024-06-07 08:46:05 +0300

    import update page.tsx

--- src/app/users/[id]/page.tsx
+++ src/app/users/[id]/page.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { User } from '../../../types/types';
 import UserDetails from '../../../components/UserDetails';
-import UserTabs from '../../../components/UserTabs';
-import { Container, CircularProgress,Box } from '@mui/material';
+import { Container} from '@mui/material';
 import { fetchUser } from '../../../lib/queries';
 
 interface UserPageProps {
